232f6af01704366562a41d33c8158e330b5d759e
Denis <godlin086@gmail.com>
2023-01-18 18:40:25 +0300

    apex charts

--- src/index.js
+++ src/index.js
@@ -10,159 +10,134 @@ Date.prototype.yyyymmdd = function() {
 
 import { useEffect, useState } from 'react'
 import { render } from 'react-dom'
-import {
-    Chart as ChartJS,
-    TimeScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js'
-import { Line } from 'react-chartjs-2/dist'
-import 'chartjs-adapter-luxon'
-import './style.css'
+import Chart from 'react-apexcharts'
 
-ChartJS.register(
-    TimeScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-)
+const lineChartOptionsTotalSpent = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ["#4318FF", "#39B8FF"],
+    markers: {
+      size: 0,
+      colors: "white",
+      strokeColors: "#7551FF",
+      strokeWidth: 3,
+      strokeOpacity: 0.9,
+      strokeDashArray: 0,
+      fillOpacity: 1,
+      discrete: [],
+      shape: "circle",
+      radius: 2,
+      offsetX: 0,
+      offsetY: 0,
+      showNullDataPoints: true,
+    },
+    tooltip: {
+      theme: "dark",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+      type: "line",
+    },
+    xaxis: {
+      type: "datetime",
+      tickAmount: 6,
+      // axisBorder: {
+      //   show: false,
+      // },
+      // axisTicks: {
+      //   show: false,
+      // },
+    },
+    yaxis: {
+      min: 0,
+    },
+    legend: {
+      show: false,
+    },
+    grid: {
+      show: false,
+      column: {
+        color: ["#7551FF", "#39B8FF"],
+        opacity: 0.5,
+      },
+    },
+    color: ["#7551FF", "#39B8FF"],
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: "dark",
+        shadeIntensity: 1,
+        opacityFrom: 0.7,
+        opacityTo: 0.9,
+        stops: [0, 100]
+      }
+    },
+}
 
 const App = () => {
-    const [store, setStore] = useState([])
-    const [active, setActive] = useState('be')
+  const [store, setStore] = useState([])
+  const [active, setActive] = useState('be')
 
-    useEffect(() => {
-        //chrome.storage.sync.clear()
-        chrome.storage.sync.get('data', getData)
-    }, [])
+  useEffect(() => {
+    //chrome.storage.sync.clear()
+    chrome.storage.sync.get('data', async (data) => {
+      let st = []
+      if (!chrome.runtime.error && data.data) {
+        st = data.data
+      }
 
-    const getData = async (data) => {
-        let st = []
-        if (!chrome.runtime.error && data.data) {
-            st = data.data
-        }
+      const day = new Date()
+      day.setDate(day.getDate() - 60)
 
-        const day = new Date()
-        day.setDate(day.getDate() - 60)
+      st = st.filter(obj => obj.date >= day.yyyymmdd())
 
-        st = st.filter(obj => obj.date >= day.yyyymmdd())
-
-        let i = 0
-        while(i < 60) {
-            if (st.findIndex(obj => obj.date === day.yyyymmdd()) === -1) {
-                const btc = await fetchCoinData('BTC', day)
-                const eth =  await fetchCoinData('ETH', day)
-                const be = parseFloat((btc/eth).toFixed(2))
-                st.push({ btc, eth, be, date: day.yyyymmdd() })
-                setStore(st)
-            }
-
-            day.setDate(day.getDate() + 1)
-            i++
-        }
-
-        chrome.storage.sync.set({ 'data': st })
-        setStore(st)
-    }
-
-    const fetchCoinData = async(coin, day) => {
+      const fetchCoinData = async (coin, day) => {
         const res = await fetch('https://api.coinbase.com/v2/prices/' + coin + '-USD/spot?date=' + day.yyyymmdd())
         const { data } = await res.json()
         return parseFloat(data.amount).toFixed(2)
-    }
+      }
 
-    const formatDate = date => {
-        const options = { month: 'long', day: 'numeric' }
-        return new Date(date).toLocaleDateString('ru-RU', options)
-    }
-     
-    const options = {
-        plugins: {
-            legend: {
-                display: false
-            },
-            tooltip: {
-                padding: 20,
-                displayColors: false,
-                callbacks: {
-                    title: function(context) {
-                        return context[0].label.substring(0, 14)
-                    },
-                    label: function(context) {
-                        let label = context.dataset.label || ''
+      let i = 0
+      while(i < 60) {
+        if (st.findIndex(obj => obj.date === day.yyyymmdd()) === -1) {
+          const btc = await fetchCoinData('BTC', day)
+          const eth =  await fetchCoinData('ETH', day)
+          const be = parseFloat((btc/eth).toFixed(2))
+          st.push({ btc, eth, be, date: day.yyyymmdd() })
+          //setStore(st)
+        }
 
-                        if (label === 'be')
-                            label = 'BTC/ETH: ' + context.parsed.y
-                        else {
-                            label = label.toUpperCase() +  ': '
-                        
-                            if (context.parsed.y !== null) {
-                                label += new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'USD' }).format(context.parsed.y)
-                            }
-                        }
-                        return label
-                    }
-                }
-            }
-        },
-        scales: {
-            x: {
-                type: 'time',
-                time: {
-                    unit: 'day'
-                    // Luxon format string
-                    //tooltipFormat: 'DD T'
-                },
-                grid: {
-                    color: 'rgb(50, 50, 50)',
-                }
-            },
-            y: {
-                grid: {
-                    color: 'rgb(50, 50, 50)',
-                },
-                beginAtZero: true,
-            },
-        },
-        parsing: {
-            yAxisKey: active,
-            xAxisKey: 'date',
-        },
-    }
+        day.setDate(day.getDate() + 1)
+        i++
+      }
 
-    return (
-        <div>
-            {store.length >= 30 &&
-                <div>
-                    <Line width={600} height={300} data={{
-                            datasets: [
-                                {
-                                    label: active,
-                                    data: store,
-                                    fill: false,
-                                    backgroundColor: 'rgb(220, 220, 220)',
-                                    borderColor: 'rgba(220, 220, 220, 0.2)',
-                                },
-                            ],
-                        }} options={options} 
-                    />
-                    <div className='buttons'>
-                        <div className={'btcbutton' + (active === 'btc' ? ' active' : '')} onClick={() => setActive('btc')}>BTC</div>
-                        <div className={'ethbutton' + (active === 'eth' ? ' active' : '')} onClick={() => setActive('eth')}>ETH</div>
-                        <div className={'bebutton' + (active === 'be' ? ' active' : '')} onClick={() => setActive('be')}>BTC/ETH</div>
-                    </div>
-                </div>
-            }
-            {store.length < 30 && <div>{store.length}</div>}
-        </div>
-    )
+      chrome.storage.sync.set({ 'data': st })
+      setStore(st)
+    })
+  }, [])
+
+  return (
+    <>
+      <Chart 
+        options={lineChartOptionsTotalSpent} 
+        series={store.length > 0 ? 
+          [{ name: active, data: store.map(i => [new Date(i.date).getTime(), i[active]]) }] 
+          : [{ data: [] }]} 
+        type='area' 
+      />
+      <div className='buttons'>
+        <div className={'btcbutton' + (active === 'btc' ? ' active' : '')} onClick={() => setActive('btc')}>BTC</div>
+        <div className={'ethbutton' + (active === 'eth' ? ' active' : '')} onClick={() => setActive('eth')}>ETH</div>
+        <div className={'bebutton' + (active === 'be' ? ' active' : '')} onClick={() => setActive('be')}>BTC/ETH</div>
+      </div>
+    </>
+  )
 }
 
 render(<App />, document.getElementById('graph'))
